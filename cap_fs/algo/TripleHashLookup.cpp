7289f630422fa8875419930c5e1f42b9e49b0d0f
Rob J Meijer <pibara@gmail.com>
2014-07-15 22:02:08 +0200

    moved some more

--- cap_fs/algo/TripleHashLookup.cpp
+++
@@ -1,9 +0,0 @@
-#include "TripleHashLookup.hpp"
-namespace capfs {
-  namespace algo {
-    TripleHashLookup::TripleHashLookup(std::string salt):mSalt(salt){}
-    TripleHashParentChild TripleHashLookup::operator[](std::string path) {
-      return TripleHashParentChild(mSalt,path);
-    }
-  }
-}
